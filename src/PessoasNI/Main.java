c2c8ed4bced1b837c10d4e76147bef60281e51d8
Filipe Ramos Pereira <filipe.ramos@unifesspa.edu.br>
2026-04-20 03:37:25 -0300

    Atividade Idade Nome

--- src/PessoasNI/Main.java
+++ src/PessoasNI/Main.java
@@ -1,4 +1,14 @@
 package PessoasNI;
 
 public class Main {
+    public static void main(String[] args) {
+
+        Pessoa ana = new Pessoa();
+
+        ana.nome = "ana";
+        ana.idade = 20;
+
+        ana.mostrarDados();
+
+    }
 }
---
+++ src/PessoasNI/Pessoa.java
@@ -0,0 +1,11 @@
+package PessoasNI;
+
+public class Pessoa {
+    public String nome;
+    public int idade;
+
+    void mostrarDados(){
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade + " Anos");
+    }
+}
